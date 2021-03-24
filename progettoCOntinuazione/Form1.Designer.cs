606d255e1e2351cf1ad9da8adb032f092b00df1c
Stud. Filippo Piumatti <f.piumatti.1365@vallauri.net>
2021-03-24 13:27:34 +0100

    progetto thread

---
+++ progettoCOntinuazione/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace progettoCOntinuazione
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnControlla = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnControllaUtente = new System.Windows.Forms.Button();
+            this.btnCerca = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 177);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(303, 186);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data2";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(276, 30);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(15, 30);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 5;
+            // 
+            // btnControlla
+            // 
+            this.btnControlla.Location = new System.Drawing.Point(375, 177);
+            this.btnControlla.Name = "btnControlla";
+            this.btnControlla.Size = new System.Drawing.Size(75, 58);
+            this.btnControlla.TabIndex = 6;
+            this.btnControlla.Text = "Controlla Elenco Log-In";
+            this.btnControlla.UseVisualStyleBackColor = true;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 84);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(461, 20);
+            this.txtPath.TabIndex = 7;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 68);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(52, 13);
+            this.lblPath.TabIndex = 8;
+            this.lblPath.Text = "Percorso:";
+            // 
+            // btnControllaUtente
+            // 
+            this.btnControllaUtente.Location = new System.Drawing.Point(465, 177);
+            this.btnControllaUtente.Name = "btnControllaUtente";
+            this.btnControllaUtente.Size = new System.Drawing.Size(75, 58);
+            this.btnControllaUtente.TabIndex = 9;
+            this.btnControllaUtente.Text = "Elenco log-in e Out utente";
+            this.btnControllaUtente.UseVisualStyleBackColor = true;
+            // 
+            // btnCerca
+            // 
+            this.btnCerca.Location = new System.Drawing.Point(15, 124);
+            this.btnCerca.Name = "btnCerca";
+            this.btnCerca.Size = new System.Drawing.Size(75, 23);
+            this.btnCerca.TabIndex = 10;
+            this.btnCerca.Text = "Cerca File";
+            this.btnCerca.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerca);
+            this.Controls.Add(this.btnControllaUtente);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnControlla);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button btnControlla;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnControllaUtente;
+        private System.Windows.Forms.Button btnCerca;
+    }
+}
+
